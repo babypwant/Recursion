28d3b3d722bb52914ef8c036489d3a731bb5aa1f
gary <baby_plant@icloud.com>
2021-03-23 11:11:17 -0700

    Working on 1

--- recursion-problems-starter-v2/starter/problems/01-sum-to-n.js
+++ recursion-problems-starter-v2/starter/problems/01-sum-to-n.js
@@ -5,14 +5,32 @@ number below 0.
 
 Examples:
 
+***********************************************************************/
+
+let sumToN = num => {
+
+  let sum = 0;
+  if (num < 0) {
+
+    return null;
+
+  } else if (num === 0) {
+
+    return sum;
+
+  } else {
+
+    sum += 1;
+
+    sumToN(num - 1);
+  }
+
+}
+
 sumToN(5) // returns 15
 sumToN(1)  // returns 1
 sumToN(9)  // returns 45
 sumToN(-8)  // returns null
-***********************************************************************/
-
-// your code here
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumToN;
