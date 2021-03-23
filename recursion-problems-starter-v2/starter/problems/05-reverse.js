fd0d3d65efcc6937809d40c40dce8f7c3cc5d4ac
gary <baby_plant@icloud.com>
2021-03-23 15:57:02 -0700

    Complete reverse

--- recursion-problems-starter-v2/starter/problems/05-reverse.js
+++ recursion-problems-starter-v2/starter/problems/05-reverse.js
@@ -4,16 +4,26 @@ it reversed.
 
 Examples:
 
-reverse("house"); // "esuoh"
-reverse("dog"); // "god"
-reverse("atom"); // "mota"
-reverse("q"); // "q"
-reverse("id"); // "di"
-reverse(""); // ""
 ***********************************************************************/
 
-// your code here
+let reverse = (string) => {
 
+  if (string.length === 0) {
+    return ''
+
+  }
+
+  return reverse(string.slice(1)) + string[0];
+
+
+}
+
+// console.log(reverse("house")); // "esuoh"
+// reverse("dog"); // "god"
+// reverse("atom"); // "mota"
+// reverse("q"); // "q"
+// reverse("id"); // "di"
+// reverse(""); // ""
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
