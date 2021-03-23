ce8216d16e1b5c09cddff4ca5ea5dc5c3bfc7b2b
gary <baby_plant@icloud.com>
2021-03-23 14:18:58 -0700

    Complete 3

--- recursion-problems-starter-v2/starter/problems/03-ice-cream-shop.js
+++ recursion-problems-starter-v2/starter/problems/03-ice-cream-shop.js
@@ -5,15 +5,30 @@ user's favorite ice cream flavor. Recursively find out whether or not the shop
 offers their favorite flavor.
 
 Examples:
-iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
-iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
 iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
 iceCreamShop(['moose tracks'], 'moose tracks'); // true
 iceCreamShop([], 'honey lavender'); // false
 ***********************************************************************/
 
+//
 
-// your code here
+
+let iceCreamShop = function (flavors, favorite) {
+  let iceCream = flavors[0];
+  if (iceCream === favorite) {
+    return true;
+  }
+  if (flavors.length === 0) {
+    return false;
+  }
+  let temp = flavors.slice(1)
+  if (temp === favorite) {
+    return true;
+  }
+  return iceCreamShop(temp, favorite)
+}
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
