573c9613289db37be0389529f9ddcc41cfd9ff8d
gary <baby_plant@icloud.com>
2021-03-23 13:55:56 -0700

    Complete 2

--- recursion-problems-starter-v2/starter/problems/02-sum-array.js
+++ recursion-problems-starter-v2/starter/problems/02-sum-array.js
@@ -5,13 +5,37 @@ include a mix of positive and negative integers!
 
 Examples:
 
-sumArray([1, 2, 3]); //  6
-sumArray([0, 1, -3]); //  -2
-sumArray([1, 2, 3, 4, 5]); // 15
+//.pop()
+
+let sumArray = (array) => {
+  let num = array[array.length - 1]
+  let sum = 0;
+  sum += num
+  if (array.length > 0) {
+    sumArray(array.pop())
+
+  }
+  return sum;
+}
 ***********************************************************************/
 
-// your code here
-  
+let sumArray = function (array) {
+
+
+  if (array.length === 0) {
+    return 0;
+  }
+  let num = array[0] + sumArray(array.slice(1))
+
+  return num;
+}
+
+
+
+
+console.log(sumArray([1, 2, 3])); //  6
+console.log(sumArray([0, 1, -3])); //  -2
+console.log(sumArray([1, 2, 3, 4, 5])); // 15
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
