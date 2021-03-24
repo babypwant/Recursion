cd056b74f642859562669ebf81806a2abaaab9bc
gary <baby_plant@icloud.com>
2021-03-24 14:08:51 -0700

    Complete is-sorted

--- recursion-problems-starter-v2/starter/problems/12-is-sorted.js
+++ recursion-problems-starter-v2/starter/problems/12-is-sorted.js
@@ -4,13 +4,58 @@ is sorted in ascending order.
 
 Examples:
 
-isSorted([1, 2, 3, 4, 5]); // true
-isSorted([1, 2, 4, 3, 5]); // false
-isSorted([2, 4, 6, 7, 8]); // true
-isSorted([5, 4, 3, 2, 1]); // false
+let isSorted = (array) => {
+
+  let start = array[0];
+
+  for (let i = 1; i < array.legth; i++) {
+    let currentNum = array[i]
+
+    if (start < currentNum) {
+
+      
+      return 
+
+    } else {
+      return false;
+    }
+
+
+  }
+
+  return true;
+
+
+}
 ***********************************************************************/
 
-// your code here
+
+let isSorted = (array) => {
+
+  let num1 = array[0];
+  let num2 = array[1];
+  let rest = array.slice(1)
+  if (array.length === 0) {
+    return true;
+  }
+
+
+  if (num1 > num2) {
+    return false;
+  } else {
+    return isSorted(rest)
+  }
+
+
+
+}
+
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
